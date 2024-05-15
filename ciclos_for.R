b1dca256d6e1ae96c7e7b914e1a3f15a47d8f537
Susana-Lefimil <susanavlr13@gmail.com>
2024-05-15 18:04:49 -0400

    Update ciclos_for.R

--- ciclos_for.R
+++ ciclos_for.R
@@ -1,1 +1,29 @@
+### Ciclos for para visualizacion de datos en un DataFrame
 
+#Ver columnas de una data
+for (col in names(data)){       
+    print(col)   #
+  #print(sum(is.na(data[[col]])))
+} 
+
+#Print columnas y sus valores
+for(i in 1:ncol(data)){
+    print(data[i])
+    #print(data[[i]])
+}
+
+#Print una columna específica, para ver sus filas
+for (i in 1:nrow(data)){   
+    print(data$columna[[i]])
+  #print(i)
+} 
+
+#Funcion para ver número de NAs por columna
+
+nas<- function(data){
+for (col in names(data)){         
+    print(col)   # data está indexada por el valor actual de "col"
+    print(sum(is.na(data[[col]])))
+ }
+}
+nas(data2)
