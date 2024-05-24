baff792e384cf553798d8e5190f648c6f49a4db4
Susana-Lefimil <susanavlr13@gmail.com>
2024-05-24 06:33:27 -0400

    Add files via upload

---
+++ dplyr.R
@@ -0,0 +1,41 @@
+mtcars
+
+##Librería dplyr
+
+library(dplyr)
+
+## Para filtrar
+
+df<-filter(mtcars, hp<70)
+df
+
+
+## Para cambiar vacíos a NA
+df[df == ""] <- NA
+
+
+## Para seleccionar algunas columnas
+
+df_1 <- mtcars %>%
+  select(cyl:drat)
+df_1
+
+
+## Para cambiar el valor de una variable con ifelse
+
+df$mpg<-ifelse(df$wt<2 | is.na(df$wt), 999, df$mpg )
+df
+
+
+## Para cambiar el valor de varias columnas con ifelse
+
+
+df_2<- mutate_at(df, c("hp", "drat", "wt"), ~ifelse(carb!=2, 999, 880))
+df_2
+
+
+## Para cambiar el valor de varias variables con replace
+
+df_3<- mutate_at(df, c("hp", "drat", "wt"), ~replace(., carb!=2, 999))
+df_3
+
